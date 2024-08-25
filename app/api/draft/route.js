852b8f9f9a12c47e10a495ebc5e2f971d736cbde
dstr89 <daniel.strmecki@gmail.com>
2024-08-25 21:39:03 +0200

    test

--- app/api/draft/route.js
+++ app/api/draft/route.js
@@ -1,5 +1,5 @@
 import { getArticle } from "@/lib/api";
-import { draftMode } from "next/headers";
+import { cookies, draftMode } from 'next/headers';
 import { redirect } from "next/navigation";
 
 export async function GET(request) {
@@ -22,5 +22,19 @@ export async function GET(request) {
   }
 
   draftMode().enable();
+
+  // Override cookie header for draft mode for usage in live-preview
+  // https://github.com/vercel/next.js/issues/49927
+  const cookieStore = cookies();
+  const cookie = cookieStore.get('__prerender_bypass');
+  cookies().set({
+    name: '__prerender_bypass',
+    value: cookie?.value,
+    httpOnly: true,
+    path: '/',
+    secure: true,
+    sameSite: 'none',
+  });
+  
   redirect(`/articles/${article.slug}`);
 }
